0a09fb9642023e283cac3b5ee24e5bea878bc005
jarmil <loncik.jakub@seznam.cz>
2013-05-18 16:31:15 +0200

    smazana main metoda

--- dtbs/src/advencedDatabase/AdvencedDatabase.java
+++ dtbs/src/advencedDatabase/AdvencedDatabase.java
@@ -20,10 +20,7 @@ public class AdvencedDatabase {
 	private SectorMap sectorMap;
 	private DatabaseHead head;
 	
-	public static void main(String[] args) {
-		System.out.print("adsdds");
-	}
-
+	
 	public AdvencedDatabase(String file) throws DatabaseException {
 		dbFileName = file;
 	}
